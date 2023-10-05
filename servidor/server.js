b616db5051aff43b98717579b7568bbde5cd1adb
Jose L. Riquelme <jlrsaez@gmail.com>
2023-10-05 16:27:24 -0300

    ennv actualizados

--- servidor/server.js
+++ servidor/server.js
@@ -2,7 +2,11 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    port: process.env.PORT,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
     connectionString: process.env.DB_URL,
     allowExitOnIdle: true
 });
